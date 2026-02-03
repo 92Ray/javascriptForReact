d3529c8b9b87ca81d43468c159b4f33ac80a5fd5
Ray <dnl1992@naver.com>
2026-02-03 14:34:14 +0900

    [추가] 함수선언문,

---
+++ section01/chapter01/chapter10.js
@@ -0,0 +1,60 @@
+// 함수선언식
+function namefunc(name){
+  console.log(name);
+}
+
+namefunc("병합null");
+let namefunc2 = namefunc;
+namefunc2("상항연산자");
+console.log(namefunc2);
+
+// 함수표현식
+let namefunc = function(name){
+  console.log(name);
+}
+namefunc("함수표현식");
+
+let namefunc = function funcf(name){
+  console.log(name);
+}
+namefunc("함수표현식");
+// 이것도 됨!
+
+let namefunc = name => console.log(name);
+namefunc("함수표현식");
+// 매개변수가 하나면 소괄호도 생략 가능(첫번째 줄의 name)
+
+let namefunc = name => name+"님 반갑습니다";
+
+let value = namefunc("함수표현식");
+console.log(value);
+
+// ---------------------------------------------------------------------------------------- //
+
+// 1. 함수 선언식 
+function funcA() { 
+  console.log("funcA"); 
+} 
+ 
+let varA = funcA; 
+varA(); 
+ 
+//2. 함수표현식(익명함수) 
+let varB = function () { 
+  console.log("funcB"); 
+}; 
+varB(); 
+ 
+let varD = function funD() { 
+   console.log("funcD"); 
+}; 
+varD();   // ok 
+funD();   // x 함수명으로 콜을 해도 에러발생, 그래서 함수표현식에서는 함수명이 필요없다.  
+ 
+// 3. 화살표 함수 
+let varC = (value) => { 
+  console.log(value); 
+  return value + 1; 
+}; 
+ 
+console.log(varC(10));

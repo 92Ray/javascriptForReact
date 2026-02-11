28b1fe7574a11b2bd6d8993c02f175cb341e02ed
Ray <dnl1992@naver.com>
2026-02-11 16:38:53 +0900

    [ 생성 ] useContext

---
+++ section12/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import '../css/Header.css'
+import { memo } from 'react';
+
+
+
+
+const Header = ({count})=> {
+  console.log(`HEADER ${count}`)
+  return <>
+  <div className="Header">
+    <h3>오늘은 달력📆</h3> 
+    <h1>{new Date().toDateString()}</h1>
+  </div>
+  </>
+}
+
+const memonizedHeader = memo(Header);
+
+export default memonizedHeader;

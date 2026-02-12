d031d05c8c99bd8a161a2abd11fedb792f143764
Ray <dnl1992@naver.com>
2026-02-12 10:18:40 +0900

    [ 생성 ] 라우터기능과 페이지 링크하는 법 세 가지 방법( a, link, useNavigate)

---
+++ section13/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = ()=> {
+
+  return<>
+    <h1>Footer 공통부분</h1>
+  </>
+
+}
+export default Footer;
---
+++ section13/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Home = ()=> {
+
+  return<>
+    <div>
+      <Header />
+    </div>
+    <h1>HOME</h1>
+    <div>
+      <Footer />
+    </div>
+  </>
+
+}
+export default Home;
